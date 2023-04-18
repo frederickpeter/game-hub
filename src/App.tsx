4a95bf620aa27172162b3cc0f076a75e3cdad27d
Frederick Peter Plange <frederick.plange@ashesi.edu.gh>
2023-04-18 11:14:01 +0000

    build a navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
-      {/* 2 rows, each with 2 columns. nav nav as row1, aside and main as row 2 */}
+      {/* on mobile, show only 2 rows each with 1 column: nav and main  */}
+      {/* on large screens show 2 rows, each with 2 columns. "nav nav" as row1, "aside main" as row 2 */}
       <Grid
         templateAreas={{
           base: `"nav" "main"`,
           lg: `"nav nav" "aside main"`,
         }}
       >
-        <GridItem area="nav" bg="coral">
-          Nav
+        <GridItem area="nav">
+          <Navbar />
         </GridItem>
+        {/* Show - allows us to control when something is visible */}
+        {/* in this case aside will only show on lg and above */}
         <Show above="lg">
           <GridItem area="aside" bg="gold">
             Aside
